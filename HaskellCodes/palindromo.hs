ce03461b53fb7aaa3be68eb4d1eea49ac5ff1776
Lucas Machado de Oliveira Andrade <andradelucasmo@gmail.com>
2021-03-26 16:49:56 -0300

    Change palindormo

--- HaskellCodes/palindromo.hs
+++ HaskellCodes/palindromo.hs
@@ -1,7 +1,20 @@
 
+corpo :: [Int] -> [Int]
+corpo (a:x)
+  |  length (a:x)>1 = [a] ++ (corpo x)
+  |  otherwise = []
+
+cabeca :: [Int] -> Int
+cabeca (c:x) = c
+
+inverte :: Int -> [Int] -> [Int]
+inverte n (i:z)
+  |  n==0  = (i:z)
+  |  otherwise = (inverte (n-1) [ b | b <- z ]) ++ [i]
+
 palindromo :: [Int] -> Bool
 palindromo [] = True
 palindromo (a:x)
-  |  x == [] = True
-  |  (a == last x) = palindromo (init x)
+  |  x==[] = True
+  |  a==cabeca(inverte (length x) (a:x)) = palindromo(corpo x) 
   |  otherwise = False
