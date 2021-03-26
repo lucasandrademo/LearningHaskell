9a8784e2fc188113235185b3c9bd69ae6782bb9f
Lucas Machado de Oliveira Andrade <andradelucasmo@gmail.com>
2021-03-26 16:57:12 -0300

    change in removerFim

--- HaskellCodes/removerFim.hs
+++ HaskellCodes/removerFim.hs
@@ -1,6 +1,11 @@
 
+corpo :: [Int] -> [Int]
+corpo (a:x)
+  |  length (a:x)>1 = [a] ++ (corpo x)
+  |  otherwise = []
+
 removerFim :: Int -> [Int] -> [Int]
 removerFim 0 (a:x) = (a:x)
 removerFim n (a:x)
   |  (n >= length x) = []
-  |  otherwise = removerFim (n - 1) ([a] ++ init x)
+  |  otherwise = removerFim (n - 1) ([a] ++ corpo x)
